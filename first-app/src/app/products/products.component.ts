fb3b4ed39061f15257514546372e1f351deb8734
Magesh Kuppan <tkmagesh77@gmail.com>
2020-03-24 14:49:14 +0530

    Adding and remove products - implementation

--- first-app/src/app/products/products.component.ts
+++ first-app/src/app/products/products.component.ts
@@ -8,18 +8,27 @@ import { Component } from '@angular/core';
         <h1>Products</h1>
         <hr>
         <label>Product Name : </label>
-        <input type="text" />
-        <input type="button" value="Add New" />
+        <input type="text" #newProductName />
+        <input type="button" value="Add New" (click)="onAddNewClick(newProductName.value)" />
         <ul>
             <li *ngFor="let productName of productNames">
                 <div>
                     <div>{{productName}}</div>
-                    <input type="button" value="Remove" />
+                    <input type="button" value="Remove" (click)="onRemoveClick(productName)" />
                 </div>
             </li>
         </ul>
     `
 })
 export class ProductsComponent{
-    productNames : string[] = [ 'Pen', 'Pencil' ];
+    productNames : string[] = [];
+
+    onAddNewClick(productName : string){
+        this.productNames.push(productName);
+    }
+
+    onRemoveClick(productToRemove : string){
+        console.log('remove button clicked ', productToRemove);
+        this.productNames.splice(this.productNames.indexOf(productToRemove), 1);
+    }
 }
