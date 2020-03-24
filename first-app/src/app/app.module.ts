99852e6d1e94a440c6a7c4c357161aecd4cf7ecf
Magesh Kuppan <tkmagesh77@gmail.com>
2020-03-24 10:00:00 +0530

    Calculator-1 implementation

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GreeterComponent } from  './greeter/greeter.component';
+import { CalculatorOneComponent } from './calculator/calculatorOne.component';
 
 
 @NgModule({
   declarations: [
     AppComponent
     , GreeterComponent
+    , CalculatorOneComponent
   ],
   imports: [
     BrowserModule
@@ -17,6 +19,7 @@ import { GreeterComponent } from  './greeter/greeter.component';
   bootstrap: [
     AppComponent
     , GreeterComponent
+    , CalculatorOneComponent
   ]
 })
 export class AppModule { }
---
+++ first-app/src/app/calculator/calculatorOne.component.ts
@@ -0,0 +1,37 @@
+import { Component } from "@angular/core";
+import { CalculatorModel } from './calculatorModel';
+
+@Component({
+    selector : 'app-calculator-1',
+    template : `
+        <br/>
+        <br/>
+        <h1>Calculator-One</h1>
+        <hr>
+        <input type="number" (input)="model.n1 = $event.target.valueAsNumber">
+        <input type="number" (input)="model.n2 = $event.target.valueAsNumber">
+        <br>
+        <input type="button" value="Add" (click)="onAddClick()">
+        <input type="button" value="Subtract" (click)="onSubtractClick()">
+        <input type="button" value="Multiply" (click)="onMultiplyClick()">
+        <input type="button" value="Divide" (click)="onDivideClick()">
+        <div>{{model.result}}</div>
+    `
+})
+export class CalculatorOneComponent{
+    
+    model : CalculatorModel = new CalculatorModel();
+
+    onAddClick(){
+        this.model.add()
+    }
+    onSubtractClick(){
+        this.model.subtract()
+    }
+    onMultiplyClick(){
+        this.model.multiply()
+    }
+    onDivideClick(){
+        this.model.divide()
+    }
+}
