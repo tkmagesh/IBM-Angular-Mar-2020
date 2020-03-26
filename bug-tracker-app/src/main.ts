c8655a8842a55a2c47e3a4fda4f04464513c0f99
Magesh Kuppan <tkmagesh77@gmail.com>
2020-03-26 12:19:40 +0530

    installing moment

--- bug-tracker-app/src/main.ts
+++ bug-tracker-app/src/main.ts
@@ -4,6 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import * as moment from 'moment';
+console.log(moment('2020-03-26T05:02:29.252Z').fromNow());
+
 if (environment.production) {
   enableProdMode();
 }
